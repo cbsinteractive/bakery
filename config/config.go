eb86c50d3cfe91a693c5b9a26cdfa64334237fb5
krystalmejia24 <krystal.mejia@cbsinteractive.com>
2020-04-15 18:44:13 -0400

    integrating xray into bakery config

--- config/config.go
+++ config/config.go
@@ -7,6 +7,8 @@ import (
 	"os"
 	"time"
 
+	"github.com/cbsinteractive/pkg/tracing"
+	"github.com/cbsinteractive/pkg/xrayutil"
 	propeller "github.com/cbsinteractive/propeller-go/client"
 	"github.com/kelseyhightower/envconfig"
 	"github.com/sirupsen/logrus"
@@ -18,7 +20,7 @@ type Config struct {
 	LogLevel   string `envconfig:"LOG_LEVEL" default:"debug"`
 	OriginHost string `envconfig:"ORIGIN_HOST"`
 	Hostname   string `envconfig:"HOSTNAME"  default:"localhost"`
-	Client     HTTPClient
+	Client
 	Propeller
 }
 
@@ -29,17 +31,20 @@ type Propeller struct {
 	Client *propeller.Client
 }
 
-// HTTPClient will issue requests to the manifest
-type HTTPClient struct {
-	Timeout time.Duration `envconfig:"CLIENT_TIMEOUT" default:"5s"`
+// Client will issue requests to the manifest
+type Client struct {
+	Timeout           time.Duration `envconfig:"CLIENT_TIMEOUT" default:"5s"`
+	EnableXRay        bool          `envconfig:"ENABLE_XRAY" default:"false"`
+	EnableXRayPlugins bool          `envconfig:"ENABLE_XRAY_PLUGINS" default:"false"`
+	Tracer            tracing.Tracer
 }
 
 // New creates a new instance of the HTTP Client
-func (h HTTPClient) New() *http.Client {
+func (c Client) New() *http.Client {
 	// https://medium.com/@nate510/don-t-use-go-s-default-http-client-4804cb19f779
-	client := &http.Client{
-		Timeout: h.Timeout,
-	}
+	client := c.Tracer.Client(&http.Client{
+		Timeout: c.Timeout,
+	})
 
 	return client
 }
@@ -52,9 +57,30 @@ func LoadConfig() (Config, error) {
 		return c, err
 	}
 
+	c.Client.init(c.GetLogger())
+
 	return c, c.Propeller.init()
 }
 
+// init will set up the tracer to track clients requests
+func (c Client) init(logger *logrus.Logger) {
+	var tracer tracing.Tracer
+	if c.EnableXRay {
+		tracer = xrayutil.XrayTracer{
+			EnableAWSPlugins: c.EnableXRayPlugins, //TODO investigate plugins
+			InfoLogFn:        logger.Infof,
+		}
+	} else {
+		tracer = tracing.NoopTracer{}
+	}
+
+	err := tracer.Init()
+	if err != nil {
+		logger.Fatalf("initializing tracer: %v", err)
+	}
+}
+
+// init will set up the propeller client to track clients requests
 func (p *Propeller) init() error {
 	if p.Host == "" || p.Creds == "" {
 		return fmt.Errorf("your Propeller configs are not set")

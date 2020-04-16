43eaa3aa20fb507c7472c021d29fbecd87f800d2
krystalmejia24 <krystal.mejia@cbsinteractive.com>
2020-04-16 17:39:04 -0400

    extending http req to make use of context

--- config/config.go
+++ config/config.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 	"net/url"
@@ -33,6 +34,7 @@ type Propeller struct {
 
 // Client will issue requests to the manifest
 type Client struct {
+	Context           context.Context
 	Timeout           time.Duration `envconfig:"CLIENT_TIMEOUT" default:"5s"`
 	EnableXRay        bool          `envconfig:"ENABLE_XRAY" default:"false"`
 	EnableXRayPlugins bool          `envconfig:"ENABLE_XRAY_PLUGINS" default:"false"`
@@ -49,6 +51,11 @@ func (c Client) New() *http.Client {
 	return client
 }
 
+// SetContext will set the context on the incoming requests
+func (c *Client) SetContext(r *http.Request) {
+	c.Context = r.Context()
+}
+
 // LoadConfig loads the configuration with environment variables injected
 func LoadConfig() (Config, error) {
 	var c Config
@@ -63,11 +70,12 @@ func LoadConfig() (Config, error) {
 }
 
 // init will set up the tracer to track clients requests
-func (c Client) init(logger *logrus.Logger) {
+func (c *Client) init(logger *logrus.Logger) {
 	var tracer tracing.Tracer
+
 	if c.EnableXRay {
 		tracer = xrayutil.XrayTracer{
-			EnableAWSPlugins: c.EnableXRayPlugins, //TODO investigate plugins
+			EnableAWSPlugins: c.EnableXRayPlugins,
 			InfoLogFn:        logger.Infof,
 		}
 	} else {
@@ -78,6 +86,8 @@ func (c Client) init(logger *logrus.Logger) {
 	if err != nil {
 		logger.Fatalf("initializing tracer: %v", err)
 	}
+
+	c.Tracer = tracer
 }
 
 // init will set up the propeller client to track clients requests
